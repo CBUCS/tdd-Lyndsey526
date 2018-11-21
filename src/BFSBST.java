0030dedeb5a332e66172d85d57725959edc408d0
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 17:13:54 -0800

    BFSBST-visit queue

--- src/BFSBST.java
+++ src/BFSBST.java
@@ -4,19 +4,39 @@ ID#684781
 
 https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
  */
+public class BFSBST<T extends Comparable<T>> extends BinarySearchTree<T> implements Visitor<T>
+{
+	public void visit()
+	{
+		Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>();
+		queue.enqueue(root);
+		visit(queue);
+		System.out.println();
+	}
 
-public class BFSBST<T extends Comparable<T>> extends BinarySearchTree<T> implements Visitor<T> {
-    public void visit() {
-        Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>();
-        queue.enqueue(root);
-        visit(queue);
-        System.out.println();
-    }
+	@Override
+	public void visit(TreeNode<T> node)
+	{
+		System.out.print(node.value + " ");
+	}
 
-    @Override
-    public void visit(TreeNode<T> node) {
-        System.out.print(node.value + " ");
-    }
+	@Override
+public void visit(Queue<TreeNode<T>> queue)
+{
+	if(queue.isEmpty())
+		return;
 
+	TreeNode<T> node = queue.dequeue();
+	if(node != null)
+	{
+		node.accept(this);
+
+		if(node.left != null);
+		queue.enqueue(node.left);
+		if(node.right != null);
+		queue.enqueue(node.right);
+
+		visit(queue);
+	}
 }
-
+}
