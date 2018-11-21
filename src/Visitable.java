489a73c4200ef46a4045c4369ca45495603c4d21
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 16:33:03 -0800

    Visitable, and Visitor classes added

---
+++ src/Visitable.java
@@ -0,0 +1,9 @@
+/*
+Lyndsey Wilson
+ID#684781
+
+ */
+public interface Visitable<T>
+{
+    public void accept(Visitor<T> v);
+}
---
+++ src/Visitor.java
@@ -0,0 +1,10 @@
+/*
+Lyndsey Wilson
+ID#684781
+
+ */
+
+public interface Visitor<T>
+{
+    public void visit(TreeNode<T> node);
+}
