dbab559392dc850d5d0611c40c3c62e2eac87c71
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 17:18:01 -0800

    BinarySearchTree Insert and TreeNode

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -8,12 +8,31 @@ https://www.geeksforgeeks.org/binary-search-tree-data-structure/
 public abstract class BinarySearchTree<T extends Comparable<T>> implements Tree<T>,java.io.Serializable
 {
 
-    public class TreeNode<T> implements java.io.Serializable
+    public abstract class BinarySearchTree<T extends Comparable<T>> implements Tree<T>
     {
+        protected TreeNode<T> root;
 
-    }
+        public BinarySearchTree()
+        {
+            root = null;
+        }
 
-    protected TreeNode<T> root;
+        //insert item in the tree at the top left most open position
+        public void insert(T value)
+        {
+            root = insertHelper(root, value);
+        }
+
+        public TreeNode<T> insertHelper(TreeNode<T> ptr, T value)
+        {
+            if(ptr == null)
+                return new TreeNode<T>(value, null, null);
+            else if(value.compareTo(ptr.value) < 0)
+                ptr.left = insertHelper(ptr.left, value);
+            else
+                ptr.right = insertHelper(ptr.right, value);
+            return ptr;
+        }
 
     public BinarySearchTree()
     {
@@ -26,10 +45,6 @@ public abstract class BinarySearchTree<T extends Comparable<T>> implements Tree<
         root = insertHelper(root, value);
     }
 
-    public TreeNode<T> insertHelper(TreeNode<T> ptr, T value)
-    {
-
-    }
 
     //removes item from tree and replaces with the bottom right most element
     public void remove(T item)
