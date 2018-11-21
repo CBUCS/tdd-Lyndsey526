ad98319a37d9d6161b4c0b4f7de746321942dcb8
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 20:57:20 -0800

    Produce getName, getPrice, getWeight, getQuanity

--- ShoppingBasket/src/Carrot.java
+++ ShoppingBasket/src/Carrot.java
@@ -3,8 +3,11 @@ Lyndsey Wilson
 ID#684781
 
  */
-public class Carrot extends Vegetable implements Visitable
-{
-    @Override
-    public double accept(Visitor v, int type) { return 0.0; }
+public class Carrot extends Vegetable implements Visitable {
+    public Carrot() {
+    }
+
+    public double accept(Visitor v, int type) {
+        return 0.0D;
+    }
 }
--- ShoppingBasket/src/Produce.java
+++ ShoppingBasket/src/Produce.java
@@ -3,15 +3,29 @@ Lyndsey Wilson
 ID#684781
 
  */
-public class Produce
-{
-    private String name;
-    private double price, weight;
-    private int quantity;
+public class Produce implements Visitable {
+    protected String name;
+    protected double price;
+    protected double weight;
+    protected int quantity;
 
-    public Produce() { }
-    public String getName() { return name; }
-    public double getPrice() { return price; }
-    public double getWeight() { return weight; }
-    public int getQuantity() { return quantity; }
+    public Produce() {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+    public int getQuantity() {
+        return this.quantity;
+    }
+    public double accept(Visitor v, int type) {
+        return type == 0 ? v.visitWeight(this) : v.visitPrice(this);
+    }
 }
