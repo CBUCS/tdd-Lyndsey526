1d15542b0d2942129b47bdda12f4a0483465fd60
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 20:44:36 -0800

    Basket added getTotalWeight, getTotalPrice

--- ShoppingBasket/src/Basket.java
+++ ShoppingBasket/src/Basket.java
@@ -14,3 +14,15 @@ public class Basket implements Visitor {
     public void addToBasket(Produce p) {
         this.list.add(p);
     }
+    public double getTotalWeight() {
+        double total = 0.0D;
+
+        for(int i = 0; i < this.list.size(); ++i) {
+            total += ((Produce)this.list.get(i)).accept(this, 0);
+        }
+
+        return total;
+    }
+    public double getTotalPrice() {
+        return 0.0D;
+    }
