9c7fffd00fedec31feaedb7bcb06320f8fa522ce
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 16:59:49 -0800

    BFSBST tree2 test

--- src/BSTTest.java
+++ src/BSTTest.java
@@ -3,6 +3,7 @@ Lyndsey Wilson
 ID#684781
 
  */
+
 public class BSTTest
 {
     public static void main(String[] args)
@@ -10,7 +11,12 @@ public class BSTTest
         DFSBST<Integer> tree = new DFSBST<Integer>();
         int values[] = {4, 2, 6, 1, 3, 5, 7};
         for(int i = 0; i < values.length; i++)
-            tree.insert(values[i], tree);
+            tree.insert(values[i]);
         tree.visit();
+
+        BFSBST<Integer> tree2 = new BFSBST<Integer>();
+        for(int i = 0; i < values.length; i++)
+            tree2.insert(values[i]);
+        tree2.visit();
     }
 }
