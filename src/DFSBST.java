f8e0087a7257fc77b029e9d772e2bea0df4149b5
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 16:27:59 -0800

    DFSBST restructred

--- src/DFSBST.java
+++ src/DFSBST.java
@@ -5,19 +5,16 @@ ID#684781
 https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
  */
 
-public class DFSBST<T extends Comparable<T>> extends BinarySearchTree<T>
+public class DFSBST<T extends Comparable<T>> extends BinarySearchTree<T> implements Visitor<T>
 {
-    //printout DFS version of the tree
+    public void visit()
+    {
+        visit(root);
+    }
+
     @Override
-    public void traverse()
+    public void visit(TreeNode<T> node)
     {
-
+        System.out.print(node.value + " ");
     }
-
-    private void DFSHelper(TreeNode<T> ptr)
-    {
-
-    }
-
-
 }
