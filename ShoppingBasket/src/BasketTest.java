cd64b17bb83c13352524042d045f267eabbabbb9
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 22:11:34 -0800

    BasketTest test adding items to the basket and print out total price and weight.

--- ShoppingBasket/src/BasketTest.java
+++ ShoppingBasket/src/BasketTest.java
@@ -3,15 +3,25 @@ Lyndsey Wilson
 ID#684781
 
  */
-public class BasketTest {
-    public BasketTest() {
-    }
-
-    public static void main(String[] args) {
+public class BasketTest
+{
+    public static void main(String args[])
+    {
         Basket basket = new Basket();
         basket.addToBasket(new Apple());
         basket.addToBasket(new Apple());
+        basket.addToBasket(new Carrot());
+        basket.addToBasket(new Corn());
+        basket.addToBasket(new Corn());
+        basket.addToBasket(new Corn());
+        basket.addToBasket(new Watermelon());
+        basket.addToBasket(new Watermelon());
+        basket.addToBasket(new Watermelon());
+        basket.addToBasket(new Watermelon());
+        basket.addToBasket(new Watermelon());
         System.out.println("Total Price: " + basket.getTotalPrice());
+        System.out.println("Total Weight: " + basket.getTotalWeight());
+        basket.getBasketList();
     }
 }
 
