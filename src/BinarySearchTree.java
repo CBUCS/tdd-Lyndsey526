817da0390d816ffe679f2d1f1809242754d45b82
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 17:23:58 -0800

    BinarySearchTree Find

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -5,84 +5,66 @@ ID#684781
 https://www.geeksforgeeks.org/insertion-binary-tree/
 https://www.geeksforgeeks.org/binary-search-tree-data-structure/
  */
-public abstract class BinarySearchTree<T extends Comparable<T>> implements Tree<T>,java.io.Serializable
-{
+public abstract class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
+    protected TreeNode<T> root;
 
-    public abstract class BinarySearchTree<T extends Comparable<T>> implements Tree<T>
-    {
-        protected TreeNode<T> root;
-
-        public BinarySearchTree()
-        {
-            root = null;
-        }
-
-        //insert item in the tree at the top left most open position
-        public void insert(T value)
-        {
-            root = insertHelper(root, value);
-        }
-
-        public TreeNode<T> insertHelper(TreeNode<T> ptr, T value)
-        {
-            if(ptr == null)
-                return new TreeNode<T>(value, null, null);
-            else if(value.compareTo(ptr.value) < 0)
-                ptr.left = insertHelper(ptr.left, value);
-            else
-                ptr.right = insertHelper(ptr.right, value);
-            return ptr;
-        }
-
-    public BinarySearchTree()
-    {
+    public BinarySearchTree() {
         root = null;
     }
 
     //insert item in the tree at the top left most open position
-    public void insert(T value)
-    {
+    public void insert(T value) {
         root = insertHelper(root, value);
     }
 
-        //removes item from tree and replaces with the bottom right most element
-        public void remove(T item) {
-            TreeNode<T> ptr = root, before = null;
-            while (ptr != null && ptr.value != item) {
-                before = ptr;
-                if (item.compareTo(ptr.value) < 0)
-                    ptr = ptr.left;
-                else
-                    ptr = ptr.right;
-            }
-            if (ptr == null)
-                return;
+    public TreeNode<T> insertHelper(TreeNode<T> ptr, T value) {
+        if (ptr == null)
+            return new TreeNode<T>(value, null, null);
+        else if (value.compareTo(ptr.value) < 0)
+            ptr.left = insertHelper(ptr.left, value);
+        else
+            ptr.right = insertHelper(ptr.right, value);
+        return ptr;
+    }
 
-            if (ptr.left == null && ptr.right == null) {
-                if (before.left == ptr)
-                    before.left = null;
-                else
-                    before.right = null;
-            } else if (ptr.left == null) {
-                if (before.left == ptr)
-                    before.left = ptr.right;
-                else
-                    before.right = ptr.right;
-            } else if (ptr.right == null) {
-                if (before.left == ptr)
-                    before.left = ptr.left;
-                else
-                    before.right = ptr.left;
-            } else {
-                TreeNode<T> max = ptr.left, beforeMax = ptr;
-                while (max.right != null) {
-                    beforeMax = max;
-                    max = max.right;
-                }
-                ptr.value = max.value;
-                beforeMax.right = max.left;
-            }
+    //removes item from tree and replaces with the bottom right most element
+    public void remove(T item) {
+        TreeNode<T> ptr = root, before = null;
+        while (ptr != null && ptr.value != item) {
+            before = ptr;
+            if (item.compareTo(ptr.value) < 0)
+                ptr = ptr.left;
+            else
+                ptr = ptr.right;
         }
+        if (ptr == null)
+            return;
+
+        if (ptr.left == null && ptr.right == null) {
+            if (before.left == ptr)
+                before.left = null;
+            else
+                before.right = null;
+        } else if (ptr.left == null) {
+            if (before.left == ptr)
+                before.left = ptr.right;
+            else
+                before.right = ptr.right;
+        } else if (ptr.right == null) {
+            if (before.left == ptr)
+                before.left = ptr.left;
+            else
+                before.right = ptr.left;
+        } else {
+            TreeNode<T> max = ptr.left, beforeMax = ptr;
+            while (max.right != null) {
+                beforeMax = max;
+                max = max.right;
+            }
+            ptr.value = max.value;
+            beforeMax.right = max.left;
+        }
+    }
 
     //find a specific tree node that contains item
     public boolean find(T item)
@@ -92,8 +74,15 @@ public abstract class BinarySearchTree<T extends Comparable<T>> implements Tree<
 
     private boolean findNodeHelper(TreeNode<T> ptr, T item)
     {
-
+        if (ptr == null)
+            return false;
+        else {
+            if (item.compareTo(ptr.value) == 0)
+                return true;
+            else if (item.compareTo(ptr.value) < 0)
+                return findNodeHelper(ptr.left, item);
+            else
+                return findNodeHelper(ptr.right, item);
+        }
     }
-
-
 }
