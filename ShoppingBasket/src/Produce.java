62bca7ebba9b704e96a9c9c972fa54c8e01be051
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 22:39:18 -0800

    Produce remove quanity function, refactor accept function

--- ShoppingBasket/src/Produce.java
+++ ShoppingBasket/src/Produce.java
@@ -6,26 +6,28 @@ ID#684781
 public class Produce implements Visitable {
     protected String name;
     protected double price;
-    protected double weight;
-    protected int quantity;
 
-    public Produce() {
-    }
-
+    public Produce() { }
     public String getName() {
         return this.name;
     }
     public double getPrice() {
         return this.price;
     }
-
     public double getWeight() {
         return this.weight;
     }
-    public int getQuantity() {
-        return this.quantity;
+
     }
-    public double accept(Visitor v, int type) {
-        return type == 0 ? v.visitWeight(this) : v.visitPrice(this);
+    @Override
+    public double accept(Visitor v, int type)
+    {
+        switch(type)
+        {
+            case 0: return v.visitWeight(this);
+            case 1: return v.visitPrice(this);
+            case 2: System.out.println(v.visitNames(this));
+        }
+        return 0.0;
     }
 }
