bb56411235f4d40b62f2eaf630df1b19f714e2d6
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 18:20:52 -0800

    Updates to visitor

--- src/Visitor.java
+++ src/Visitor.java
@@ -6,8 +6,8 @@ ID#684781
 
 import java.util.Queue;
 
-public interface Visitor<T>
-{
+public interface Visitor<T> {
     public void visit(TreeNode<T> node);
 
     public void visit(Queue<TreeNode<T>> queue);
+}
