0b6f98c2c2beb42d6cf6c3426993560c4ff4e618
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 16:39:03 -0800

    TreeNode TreeNode Value, left, right

---
+++ src/TreeNode.java
@@ -0,0 +1,24 @@
+/*
+Lyndsey Wilson
+ID#684781
+
+ */
+public class TreeNode<T> implements Visitable<T>
+{
+    T value;
+    TreeNode<T> left, right;
+
+    public TreeNode(T val, TreeNode<T> l, TreeNode<T> r)
+    {
+        value = val;
+        left = l;
+        right = r;
+    }
+
+    @Override
+    public void accept(Visitor<T> v)
+    {
+        v.visit(this);
+    }
+}
+
