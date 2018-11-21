410704e16ee4e015e7960a9a8d60483670998ee4
Lyndsey526 <lyndsey.bryanne.wilson@calbaptist.edu>
2018-11-20 20:14:28 -0800

    ShoppingBasket and classes

---
+++ ShoppingBasket/src/Apple.java
@@ -0,0 +1,11 @@
+/*
+Lyndsey Wilson
+ID#684781
+
+ */
+public class Apple extends Fruit implements Visitable {
+    @Override
+    public double accept(Visitor v, int type) {
+        return 0.0;
+    }
+}
